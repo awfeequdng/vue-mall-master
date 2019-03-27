2dcfdc820900ed5e5a8ae8d6c78fa2a25e3a7291
ST000286 <libangzhu@cyou-inc.com>
2019-03-27 17:24:40 +0800

    Initial commit

---
+++ server/config/sequelizeBase.js
@@ -0,0 +1,8 @@
+//sequelize基础配置文件
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('mall','root','123456',{
+	host:'localhost',
+	dialect:'mysql',
+})
+
+module.exports = sequelize;
